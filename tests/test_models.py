24adf5f72c1d76c0c391bfe2fd8552821c580968
Sichen Zhong <sichenzhong@ucsb.edu>
2025-12-09 18:50:30 -0500

    fix

--- tests/test_models.py
+++ tests/test_models.py
@@ -136,16 +136,16 @@ class TestInventoryModel(TestCase):
         self.assertEqual(updated.condition, Condition.USED)
         self.assertNotEqual(updated.condition, old_condition)
 
-    # def test_update_inventory_item_with_error(self):
-    #     """It should raise DataValidationError when update() fails"""
-    #     item = InventoryFactory()
-    #     item.create()
-    #     self.assertIsNotNone(item.id)
+    def test_update_inventory_item_with_error(self):
+        """It should raise DataValidationError when update() fails due to database error"""
+        item = InventoryFactory()
+        item.create()
+        self.assertIsNotNone(item.id)
         
-    #     # Mock the commit to raise an exception
-    #     with patch('service.models.db.session.commit', side_effect=Exception("Simulated DB error")):
-    #         with self.assertRaises(DataValidationError):
-    #             item.update()
+        # Mock db.session.commit to simulate a database error
+        with patch('service.models.db.session.commit', side_effect=Exception("Database error")):
+            with self.assertRaises(DataValidationError):
+                item.update()
 
     # ----------------------------------------------------------
     # TEST DELETE
